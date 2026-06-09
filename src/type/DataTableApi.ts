7ed55461c56b4572b5af21c99c4973cb438a3672
Gleb <gsevostianov@hse.ru>
2026-06-09 14:46:14 +0300

    Старт

---
+++ src/type/DataTableApi.ts
@@ -0,0 +1,13 @@
+﻿export interface Columns {
+    columns: ColumnInfo[]
+    totalRows: number
+}
+
+export interface ColumnInfo {
+    columnName: string;
+    dataType: string
+}
+
+export interface RowData {
+    data: Record<string, string |number>
+}
